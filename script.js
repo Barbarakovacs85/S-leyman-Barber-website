b204fb0005bb1d69604e2adf2ee005832c19ce46
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2024-04-26 22:52:42 +0200

    add javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    }else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler");
+const navLinks = document.querySelectorAll("fdata-nav-Links");
+
+const toggleNavbar = () => navbar.classList.toggle("active");
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = () => navbar.classList.remove("active");
+
+addEventOnElem(navLinks, "click", closeNavbar);
